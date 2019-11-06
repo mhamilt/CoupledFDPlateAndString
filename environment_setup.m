d6f7592b31c916fa81693988c789af917e58705f
mhamilt <matt.hamilton@ed.ac.uk>
2019-11-06 09:52:50 +0000

    change to correct file path

--- environment_setup.m
+++ environment_setup.m
@@ -3,6 +3,6 @@
 % add to path the required folder that included dependent instrument files and functions
 %
 
-addpath('/functions/');
-addpath('/midi/');
-addpath('/calibration/');
+addpath('functions/');
+addpath('midi/');
+addpath('calibration/');
